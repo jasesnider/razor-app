000a8957601eb50a31280342b0061102be37027c
Jeff Snider <jeff.snider@jaseone.com>
2021-01-24 20:22:38 -0600

    Initial commit.

---
+++ JaseOne/Data/JaseOneContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JaseOne.Models;
+
+namespace JaseOne.Data
+{
+    public class JaseOneContext : DbContext
+    {
+        public JaseOneContext (DbContextOptions<JaseOneContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JaseOne.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ JaseOne/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using JaseOne.Data;
+using JaseOne.Models;
+
+namespace JaseOne.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly JaseOne.Data.JaseOneContext _context;
+
+        public IndexModel(JaseOne.Data.JaseOneContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
